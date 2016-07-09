6467f41c260a08cd42fff102e1e1aa82a33c092e
Ryper_Zsolt <akormanyzsolt@gmail.com>
2016-07-09 19:48:43 +0200

    ActivityTimer beta

---
+++ ActivityTimer/ActivityTimer.Designer.cs
@@ -0,0 +1,122 @@
+﻿namespace ActivityTimer
+{
+    partial class ActivityTimer
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timerSecond = new System.Windows.Forms.Timer(this.components);
+            this.timerFullTime = new System.Windows.Forms.Timer(this.components);
+            this.bStart = new System.Windows.Forms.Button();
+            this.bPause = new System.Windows.Forms.Button();
+            this.bStop = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.lRemainingTime = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // bStart
+            // 
+            this.bStart.Location = new System.Drawing.Point(25, 165);
+            this.bStart.Name = "bStart";
+            this.bStart.Size = new System.Drawing.Size(75, 23);
+            this.bStart.TabIndex = 0;
+            this.bStart.Text = "Start";
+            this.bStart.UseVisualStyleBackColor = true;
+            // 
+            // bPause
+            // 
+            this.bPause.Location = new System.Drawing.Point(137, 165);
+            this.bPause.Name = "bPause";
+            this.bPause.Size = new System.Drawing.Size(75, 23);
+            this.bPause.TabIndex = 1;
+            this.bPause.Text = "Pause";
+            this.bPause.UseVisualStyleBackColor = true;
+            // 
+            // bStop
+            // 
+            this.bStop.Location = new System.Drawing.Point(248, 165);
+            this.bStop.Name = "bStop";
+            this.bStop.Size = new System.Drawing.Size(75, 23);
+            this.bStop.TabIndex = 2;
+            this.bStop.Text = "Stop";
+            this.bStop.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(137, 77);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(45, 20);
+            this.textBox1.TabIndex = 3;
+            // 
+            // lRemainingTime
+            // 
+            this.lRemainingTime.AutoSize = true;
+            this.lRemainingTime.Location = new System.Drawing.Point(14, 54);
+            this.lRemainingTime.Name = "lRemainingTime";
+            this.lRemainingTime.Size = new System.Drawing.Size(86, 13);
+            this.lRemainingTime.TabIndex = 4;
+            this.lRemainingTime.Text = "Remaining Time:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(177, 54);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "label2";
+            // 
+            // ActivityTimer
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lRemainingTime);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.bStop);
+            this.Controls.Add(this.bPause);
+            this.Controls.Add(this.bStart);
+            this.Name = "ActivityTimer";
+            this.Size = new System.Drawing.Size(354, 208);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timerSecond;
+        private System.Windows.Forms.Timer timerFullTime;
+        private System.Windows.Forms.Button bStart;
+        private System.Windows.Forms.Button bPause;
+        private System.Windows.Forms.Button bStop;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label lRemainingTime;
+        private System.Windows.Forms.Label label2;
+    }
+}

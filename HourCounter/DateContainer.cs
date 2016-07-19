9eb1ea932023f19ba1ca51efd8526fd738aace38
Ryper_Zsolt <akormanyzsolt@gmail.com>
2016-07-19 18:21:01 +0200

    DateContainer new beta class

---
+++ HourCounter/DateContainer.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HourCounter
+{
+    //class DateContainer
+    //{
+    //    map<day, sortedList<date, ActivityDesc desc>>
+    //    Dictionary< string , SortedList<string, ActivityDesc> > asd; 
+    //    Set activityDescContainer<ActivityDesc>
+    //}
+
+    //class ActivityDesc
+    //{
+    //    Activity _parent;
+    //    String _description;
+    //}
+    
+    ///TODO
+    //Beepiteni: map<day, sortedList<date, activityDesc desc>> //Napon miket csináltak
+    //Map< string activityName , sortedList  <date, activityDesc Desc> > //Activity-t ideje részletesen miből tevődik ki
+    //Map<ActivityDesc, int (how many times )> //Hanyszor csinálták ezt az activityt - Durva ötlet, de ezt inkább szokások számlálásánál kell majd máshogy megoldani!
+    //ActivityDesc:
+    //Osztály mely tartalmazza a szülő activity-t tehát hogy melyikhez tartozik, és tartalmazza, hogy:
+    //Leírás hogy mit csinált részletesen
+
+}

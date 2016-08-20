c99cbe8f993d7f1f5674b1e88dd2be0d21c4ae99
Ryper_Zsolt <akormanyzsolt@gmail.com>
2016-08-20 20:43:31 +0200

    Settings skeleton added

---
+++ Utils/Settings.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Utils
+{
+    public class Settings
+    {
+        private Settings ()
+        {
+        }
+        static private Settings _instance = null;
+        static public Settings Get //Singleton
+        {
+            get
+            {
+                if (_instance == null)
+                    _instance = new Settings ();
+
+                return _instance;
+            }
+            private set { }
+        }
+
+        public GeneralSettings General;
+
+        public class GeneralSettings
+        {
+            public bool isGDriveSave;
+        }
+    }
+}

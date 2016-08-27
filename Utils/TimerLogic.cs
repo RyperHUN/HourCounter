a7938c5f6883edf397f289b2cc2bcb56db0f01c7
Ryper_Zsolt <akormanyzsolt@gmail.com>
2016-08-27 13:06:14 +0200

    TimerLogic - Added Timer + functions + events for using

--- Utils/TimerLogic.cs
+++ Utils/TimerLogic.cs
@@ -3,19 +3,83 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace Utils
 {
     public class TimerLogic
     {
-        public  Time InitTime   = new Time ();
-        public  Time StartTime  = new Time ();
-        private Time RemainTime = new Time ();
+        private  Timer timer       = new Timer ();
+        private  Time _initTime    = new Time ();
+        private  Time _startTime   = new Time ();
+        private  Time _remainTime  = new Time ();
+        public bool isValidTimeSet = false;
+
+        public TimerLogic ()
+        {
+            timer.Interval  = 1000;
+            timer.Tick      += timeChanged;
+            TimerEndedEvent += TimerEndedHandler;
+        }
+
+        public Time InitTime
+        {
+            get { return _initTime; }
+            set
+            {
+                _initTime      = value;
+                isValidTimeSet = true;
+            }
+        }
+        public Time RemainTime
+        {
+            get { return _remainTime; }
+        }
 
         public Time GetElapsedTime ()
         {
-            return new Time (StartTime.Seconds - RemainTime.Seconds);
+            return new Time (_startTime.Seconds - _remainTime.Seconds);
         }
+
+        public void Start ()
+        {
+            _startTime = new Time(_initTime);
+            TimeChangedEvent.Invoke ();
+            timer.Start ();
+        }
+
+        public void Pause ()
+        {
+            timer.Stop ();
+        }
+
+        public void Continue ()
+        {
+            timer.Start ();
+        }
+
+        public void Stop ()
+        {
+            TimerEndedEvent.Invoke ();
+        }
+
+        private void timeChanged (object sender, EventArgs e)
+        {
+            _remainTime = new Time (_remainTime.Seconds - 1);
+            TimeChangedEvent.Invoke ();
+            
+            if (_remainTime.Seconds == 0)
+                TimerEndedEvent.Invoke ();
+        }
+
+        private void TimerEndedHandler ()
+        {
+            timer.Stop ();
+        }
+
+        public delegate void TimeChange ();
+        public event TimeChange TimeChangedEvent;
+        public event TimeChange TimerEndedEvent;
 
         //Timer start event
         //Timer end event

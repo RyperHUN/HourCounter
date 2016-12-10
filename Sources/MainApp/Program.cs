581e192df72ab1baed45ea3b7c2a82d618fcab81
RyperHUN <akormanyzsolt@gmail.com>
2016-12-10 18:32:47 +0100

    Added another instance checking when application starts

--- Sources/MainApp/Program.cs
+++ Sources/MainApp/Program.cs
@@ -14,12 +14,20 @@ namespace HourCounter
         [STAThread]
         static void Main()
         {
+            bool result;
+            var mutex = new System.Threading.Mutex(true, "df29c248-e00e-4f87-8ada-7d72b2d7ad31", out result); //Unique ID for app
 
-
+            if (!result)
+            {
+                MessageBox.Show("Another instance is already running.");
+                return;
+            }
 
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new App());
+
+            GC.KeepAlive(mutex); //This way GC Don't collect the mutex
         }
     }
 }

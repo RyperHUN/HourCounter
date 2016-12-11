0c6bdb5623869fec7e526e77be6c477099e2c429
RyperHUN <akormanyzsolt@gmail.com>
2016-12-11 22:32:14 +0100

    Fixed to IComparable

--- Sources/Dependencies/TimeUtils/OnlyDate.cs
+++ Sources/Dependencies/TimeUtils/OnlyDate.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Utils
 {
-    public class OnlyDate : IComparer<OnlyDate>
+    public class OnlyDate : IComparable<OnlyDate>
     {
         public int Year { get; private set; }
         public int Month { get; private set; }
@@ -23,11 +23,12 @@ namespace Utils
             return new OnlyDate (date.AddDays (days));            
         }
         //Returns 0 if identical, +num if lhs, -Num if rhs
-        public int Compare (OnlyDate lhs, OnlyDate rhs)
+
+        public int CompareTo (OnlyDate rhs)
         {
-            int year = lhs.Year   - rhs.Year;
-            int month = lhs.Month - rhs.Month;
-            int day  = lhs.Day - rhs.Day;
+            int year = Year   - rhs.Year;
+            int month = Month - rhs.Month;
+            int day  = Day - rhs.Day;
             if (year == 0)
             {
                 if (month == 0)

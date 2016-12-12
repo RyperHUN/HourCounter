15a0f8c1fc0bffe51ac3fb6f1715942928da3c9f
RyperHUN <akormanyzsolt@gmail.com>
2016-12-12 21:13:04 +0100

    Refactored day chooser

--- Sources/Dependencies/Activity/Activity.cs
+++ Sources/Dependencies/Activity/Activity.cs
@@ -87,7 +87,7 @@ namespace HourCounter
                 if (!loadedTime) //If full file load was unsucessful
                     throw;
             }
-            _dailyTime.Add(new OnlyDate(DateTime.Now.AddDays(-1)), new Time(6000)); ///TODO Torolni
+            _dailyTime.Add(new OnlyDate(DateTime.Now.AddDays(-1)), new Time(6000)); ///TODO Delete
             _dailyTime.Add(new OnlyDate(DateTime.Now.AddDays(0)), new Time(16000));
         }
     //Vegigmegy az osszes subActivityn es hozzáadja az ő idejüket a Counterhez, és ezt fogja visszaadni mint össz idő.
@@ -215,10 +215,7 @@ namespace HourCounter
         public string getFormatedStatus()
         {
             if (_isDateSettingOn)
-                if (_savedDateTo == null)
-                    return _name + "    " + CounterDate (_savedDateFrom).Hours + "h";
-                else
-                    return _name + "    " + CounterDate (_savedDateFrom, _savedDateTo).Hours + "h";
+                return _name + "    " + CounterDate (_savedDateFrom, _savedDateTo).Hours + "h";
             else
                 return _name + "    " + Counter.Hours + "h";
         }
@@ -352,43 +349,48 @@ namespace HourCounter
     }
 
     public static class SortedListExtensions
-{
-    public static int BinarySearch<TKey, TValue>(this SortedList<TKey, TValue> sortedList, TKey keyToFind, IComparer<TKey> comparer = null)
     {
-        // need to create an array because SortedList.keys is a private array
-        var keys = sortedList.Keys;
-        TKey[] keyArray = new TKey[keys.Count];
-        for (int i = 0; i < keyArray.Length; i++)
-            keyArray[i] = keys[i];
+        public static int BinarySearch<TKey, TValue>(this SortedList<TKey, TValue> sortedList, TKey keyToFind, IComparer<TKey> comparer = null)
+        {
+            // need to create an array because SortedList.keys is a private array
+            var keys = sortedList.Keys;
+            TKey[] keyArray = new TKey[keys.Count];
+            for (int i = 0; i < keyArray.Length; i++)
+                keyArray[i] = keys[i];
 
-        if(comparer == null) comparer = Comparer<TKey>.Default;
-        int index = Array.BinarySearch<TKey>(keyArray, keyToFind, comparer);
-        return index;
+            if(comparer == null) comparer = Comparer<TKey>.Default;
+            int index = Array.BinarySearch<TKey>(keyArray, keyToFind, comparer);
+            return index;
+        }
+
+        public static IEnumerable<TKey> GetKeyRangeBetween<TKey, TValue>(this SortedList<TKey, TValue> sortedList, TKey low, TKey high, IComparer<TKey> comparer = null)
+        {
+            int lowIndex = sortedList.BinarySearch(low, comparer);
+            if (lowIndex < 0)
+            {
+                // list doesn't contain the key, find nearest behind
+                // If not found, BinarySearch returns the complement of the index
+                lowIndex = ~lowIndex;
+            }
+
+            int highIndex = sortedList.BinarySearch(high, comparer);
+            if (highIndex < 0)
+            {
+                // list doesn't contain the key, find nearest before
+                // If not found, BinarySearch returns the complement of the index
+                highIndex = ~highIndex - 1;
+            }
+
+            var keys = sortedList.Keys;
+            for (int i = lowIndex; i <= highIndex; i++)
+            {
+                yield return keys[i];
+            }
+        }
     }
 
-    public static IEnumerable<TKey> GetKeyRangeBetween<TKey, TValue>(this SortedList<TKey, TValue> sortedList, TKey low, TKey high, IComparer<TKey> comparer = null)
+    public static class DayChooserHelper
     {
-        int lowIndex = sortedList.BinarySearch(low, comparer);
-        if (lowIndex < 0)
-        {
-            // list doesn't contain the key, find nearest behind
-            // If not found, BinarySearch returns the complement of the index
-            lowIndex = ~lowIndex;
-        }
 
-        int highIndex = sortedList.BinarySearch(high, comparer);
-        if (highIndex < 0)
-        {
-            // list doesn't contain the key, find nearest before
-            // If not found, BinarySearch returns the complement of the index
-            highIndex = ~highIndex - 1;
-        }
-
-        var keys = sortedList.Keys;
-        for (int i = lowIndex; i <= highIndex; i++)
-        {
-            yield return keys[i];
-        }
     }
-}
 }
